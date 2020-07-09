a426b687f0d7e3b7d522d1bedbb62171dd9092ad
febri.triakbar <febri.triakbar@yahoo.com>
2020-07-09 07:00:12 +0700

    npm run build

---
+++ build/precache-manifest.616b286c6a4ff2955231c73ddabb42cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4381dd23d70bfd0b993f3d92e2623adc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a19cb645bfb5efc03ed7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "00a1bf134f280d38aa3e",
+    "url": "/static/css/main.3ce0c0f4.chunk.css"
+  },
+  {
+    "revision": "a19cb645bfb5efc03ed7",
+    "url": "/static/js/2.c174eab4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c174eab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a1bf134f280d38aa3e",
+    "url": "/static/js/main.28c583d2.chunk.js"
+  },
+  {
+    "revision": "5338d34a451b12eb8af0",
+    "url": "/static/js/runtime-main.1ee8bc80.js"
+  },
+  {
+    "revision": "c9423772a2f3624648fe49a4224489de",
+    "url": "/static/media/logo.c9423772.png"
+  }
+]);
